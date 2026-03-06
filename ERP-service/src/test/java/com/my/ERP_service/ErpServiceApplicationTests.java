e74b929247d5f9a0b8c43d3a959b85adc5c23459
Thomas <thomaschou9177@gmail.com>
2026-03-06 19:59:39 +0800

    Initial ERP project setup from Spring Initializr

---
+++ ERP-service/src/test/java/com/my/ERP_service/ErpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.ERP_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

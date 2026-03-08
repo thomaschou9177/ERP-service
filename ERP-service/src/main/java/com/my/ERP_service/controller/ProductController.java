dcccbf7b67909bc8aa79615778a335f4eeb0486d
Thomas <thomaschou9177@gmail.com>
2026-03-08 19:09:07 +0800

    fix network error by removing block from spring security

--- ERP-service/src/main/java/com/my/ERP_service/controller/ProductController.java
+++ ERP-service/src/main/java/com/my/ERP_service/controller/ProductController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin("*") // Allows your React app to talk to this API
+@CrossOrigin(origins = "http://localhost:3000") // Allows your React app to talk to this API
 public class ProductController {
 
     @Autowired
